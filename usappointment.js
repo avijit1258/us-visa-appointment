3aa45b65d7a4562a4aa73b2eabad6c636b63029a
Avijit Bhattacharjee <avijitbhatt1258@gmail.com>
2023-10-02 17:21:42 -0700

    check selected date for reschedule

--- usappointment.js
+++ usappointment.js
@@ -321,7 +321,9 @@ const axios = require("axios");
       if (firstDate > currentDate) {
         log(
           "There is not an earlier date available than " +
-            currentDate.toISOString().slice(0, 10) + " earliest availability: " + firstDate.toISOString().slice(0, 10)
+            currentDate.toISOString().slice(0, 10) +
+            " earliest availability: " +
+            firstDate.toISOString().slice(0, 10)
         );
         await browser.close();
         return false;
@@ -332,72 +334,154 @@ const axios = require("axios");
 
     //Go to appointment page
     {
-        const targetPage = page;
-        await targetPage.goto('https://ais.usvisa-info.com/en-' + region + '/niv/schedule/' + appointmentId + '/appointment', { waitUntil: 'domcontentloaded' });
-        await sleep(1000);
+      const targetPage = page;
+      await targetPage.goto(
+        "https://ais.usvisa-info.com/en-" +
+          region +
+          "/niv/schedule/" +
+          appointmentId +
+          "/appointment",
+        { waitUntil: "domcontentloaded" }
+      );
+      await sleep(1000);
     }
 
     //Select multiple people if it is a group appointment
     {
-        if(groupAppointment){
-          const targetPage = page;
-          const element = await waitForSelectors([["aria/Continue"],["#main > div.mainContent > form > div:nth-child(3) > div > input"]], targetPage, { timeout, visible: true });
-          await scrollIntoViewIfNeeded(element, timeout);
-          await element.click({ offset: { x: 70.515625, y: 25.25} });
-          await sleep(1000);
-        }
+      if (groupAppointment) {
+        const targetPage = page;
+        const element = await waitForSelectors(
+          [
+            ["aria/Continue"],
+            ["#main > div.mainContent > form > div:nth-child(3) > div > input"],
+          ],
+          targetPage,
+          { timeout, visible: true }
+        );
+        await scrollIntoViewIfNeeded(element, timeout);
+        await element.click({ offset: { x: 70.515625, y: 25.25 } });
+        await sleep(1000);
+      }
     }
 
     //Select the specified consular from the dropdown
     {
-        const targetPage = page;
-        const element = await waitForSelectors([["aria/Consular Section Appointment","aria/[role=\"combobox\"]"],["#appointments_consulate_appointment_facility_id"]], targetPage, { timeout, visible: true });
-        await scrollIntoViewIfNeeded(element, timeout);
-        await page.select("#appointments_consulate_appointment_facility_id", consularId);
-        await sleep(1000);
+      const targetPage = page;
+      const element = await waitForSelectors(
+        [
+          ["aria/Consular Section Appointment", 'aria/[role="combobox"]'],
+          ["#appointments_consulate_appointment_facility_id"],
+        ],
+        targetPage,
+        { timeout, visible: true }
+      );
+      await scrollIntoViewIfNeeded(element, timeout);
+      await page.select(
+        "#appointments_consulate_appointment_facility_id",
+        consularId
+      );
+      await sleep(1000);
     }
 
     //Click on date input
     {
-        const targetPage = page;
-        const element = await waitForSelectors([["aria/Date of Appointment *"],["#appointments_consulate_appointment_date"]], targetPage, { timeout, visible: true });
-        await scrollIntoViewIfNeeded(element, timeout);
-        await element.click({ offset: { x: 394.5, y: 17.53125} });
-        await sleep(1000);
+      const targetPage = page;
+      const element = await waitForSelectors(
+        [
+          ["aria/Date of Appointment *"],
+          ["#appointments_consulate_appointment_date"],
+        ],
+        targetPage,
+        { timeout, visible: true }
+      );
+      await scrollIntoViewIfNeeded(element, timeout);
+      await element.click({ offset: { x: 394.5, y: 17.53125 } });
+      await sleep(1000);
     }
 
     //Keep clicking next button until we find the first available date and click to that date
     {
-        const targetPage = page;
-        while (true) {
-          try {
-            const element = await waitForSelectors([["aria/25[role=\"link\"]"],["#ui-datepicker-div > div.ui-datepicker-group.ui-datepicker-group > table > tbody > tr > td.undefined > a"]], targetPage, { timeout:smallTimeout, visible: true });
+      const targetPage = page;
+      while (true) {
+        try {
+          const element = await waitForSelectors(
+            [
+              ['aria/25[role="link"]'],
+              [
+                "#ui-datepicker-div > div.ui-datepicker-group.ui-datepicker-group > table > tbody > tr > td.undefined > a",
+              ],
+            ],
+            targetPage,
+            { timeout: smallTimeout, visible: true }
+          );
+          await scrollIntoViewIfNeeded(element, timeout);
+          await page.click(
+            "#ui-datepicker-div > div.ui-datepicker-group.ui-datepicker-group > table > tbody > tr > td.undefined > a"
+          );
+          await sleep(500);
+          break;
+        } catch (err) {
+          {
+            const targetPage = page;
+            const element = await waitForSelectors(
+              [
+                ["aria/Next", 'aria/[role="generic"]'],
+                [
+                  "#ui-datepicker-div > div.ui-datepicker-group.ui-datepicker-group-last > div > a > span",
+                ],
+              ],
+              targetPage,
+              { timeout, visible: true }
+            );
             await scrollIntoViewIfNeeded(element, timeout);
-            await page.click('#ui-datepicker-div > div.ui-datepicker-group.ui-datepicker-group > table > tbody > tr > td.undefined > a');
-            await sleep(500);
-            break;
-          } catch (err) {
-            {
-                const targetPage = page;
-                const element = await waitForSelectors([["aria/Next","aria/[role=\"generic\"]"],["#ui-datepicker-div > div.ui-datepicker-group.ui-datepicker-group-last > div > a > span"]], targetPage, { timeout, visible: true });
-                await scrollIntoViewIfNeeded(element, timeout);
-                await element.click({ offset: { x: 4, y: 9.03125} });
-            }
+            await element.click({ offset: { x: 4, y: 9.03125 } });
           }
         }
+      }
     }
 
     //Select the first available Time from the time dropdown
     {
-        const targetPage = page;
-        const element = await waitForSelectors([["#appointments_consulate_appointment_time"]], targetPage, { timeout, visible: true });
-        await scrollIntoViewIfNeeded(element, timeout);
-        await page.evaluate(() => {
-          document.querySelector('#appointments_consulate_appointment_time option:nth-child(2)').selected = true;
-          const event = new Event('change', {bubbles: true});
-          document.querySelector('#appointments_consulate_appointment_time').dispatchEvent(event);
-        })
-        await sleep(1000);
+      const targetPage = page;
+      const element = await waitForSelectors(
+        [["#appointments_consulate_appointment_time"]],
+        targetPage,
+        { timeout, visible: true }
+      );
+      await scrollIntoViewIfNeeded(element, timeout);
+      await page.evaluate(() => {
+        document.querySelector(
+          "#appointments_consulate_appointment_time option:nth-child(2)"
+        ).selected = true;
+        const event = new Event("change", { bubbles: true });
+        document
+          .querySelector("#appointments_consulate_appointment_time")
+          .dispatchEvent(event);
+      });
+      await sleep(1000);
+    }
+
+    // check the date we selected
+    {
+      const targetPage = page;
+      const selectedDateElement = await waitForSelector(
+        ["#appointments_consulate_appointment_date div"],
+        targetPage,
+        { timeout, visible: true }
+      );
+      const dateSelectedStr = await selectedDateElement.evaluate((el) => el.textContent);
+      const dateSelected = new Date(dateSelectedStr);
+
+      log("Selected date for reschedule: " + dateSelectedStr);
+      
+      if (dateSelected > currentDate) {
+        log(
+          "Selected date is not the date we wanted. Selected date: " +
+            dateSelected.toISOString().slice(0, 10)
+        );
+        await browser.close();
+        return false;
+      }
     }
 
     //Click on reschedule button
